b6b0c85b11d47b19445c271769fb1985e16b5e1d
Mehdi <zaiicko7@proton.me>
2025-05-12 23:33:00 +0200

    Add : ft_memset all data at NULL

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lowatell <lowatell@student.s19.be>         +#+  +:+       +#+        */
+/*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 13:29:57 by zaiicko           #+#    #+#             */
-/*   Updated: 2025/05/12 01:57:33 by lowatell         ###   ########.fr       */
+/*   Updated: 2025/05/12 23:28:38 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,11 @@ int	main(int ac, char **av)
 {
 	void	*mlx;
 	void	*win;
+	t_data	data;
 
 	(void)ac;
 	(void)av;
+	ft_memset(&data, 0, sizeof(t_data));
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 200, 200, "test");
 	if (!win)
