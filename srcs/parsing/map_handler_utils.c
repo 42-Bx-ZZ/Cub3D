54b638461d2a11b6d5d5e09670a9b577ab201ed8
Mehdi <zaiicko7@proton.me>
2025-06-20 14:38:30 +0200

    Fix : error msg when '\n' in and after map

--- srcs/parsing/map_handler_utils.c
+++ srcs/parsing/map_handler_utils.c
@@ -6,7 +6,7 @@
 /*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 03:41:56 by zaiicko           #+#    #+#             */
-/*   Updated: 2025/06/18 03:42:46 by zaiicko          ###   ########.fr       */
+/*   Updated: 2025/06/20 14:36:33 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ int	count_map_lenght(t_data *data, int i)
 			i++;
 			len++;
 		}
-		else if (data->cub_file[i][0] == 0 && data->cub_file[i + 1])
+		else if (data->cub_file[i][0] == 0)
 			free_all_and_print_exit(data,
-				"Error\nMap can't be separeted by '\\n'\n");
+				"Error\n'\\n' in the map section\n");
 		else
 			break ;
 	}
