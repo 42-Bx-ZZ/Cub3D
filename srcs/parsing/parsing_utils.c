bd65ae94c467fdec6e7d86e9f89fd705f850accd
Mehdi <zaiicko7@proton.me>
2025-06-20 13:22:30 +0200

    Chore : move functions for extract the cub file data in cub_file_handler.c

--- srcs/parsing/parsing_utils.c
+++ srcs/parsing/parsing_utils.c
@@ -6,54 +6,8 @@
 /*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 17:02:22 by zaiicko           #+#    #+#             */
-/*   Updated: 2025/05/29 15:09:36 by zaiicko          ###   ########.fr       */
+/*   Updated: 2025/06/20 13:18:51 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3d.h"
-
-static int	count_file_lines(char *file)
-{
-	int		fd;
-	char	*line;
-	int		i;
-
-	i = 0;
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		ft_print_exit("Error\nCan't open .cub file");
-	line = get_next_line(fd);
-	while (line)
-	{
-		i++;
-		free(line);
-		line = get_next_line(fd);
-	}
-	close(fd);
-	return (i);
-}
-
-void	extract_all_cub_data(t_data *data, char *file)
-{
-	int		fd;
-	int		i;
-
-	data->cub_file = (char **)malloc(sizeof(char *)
-			* (count_file_lines(file) + 1));
-	if (!data->cub_file)
-		ft_print_exit("Error\nMalloc failed\n");
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		free_all_and_print_exit(data, "Error\nCan't open .cub file");
-	i = 0;
-	data->cub_file[i] = get_next_line(fd);
-	while (data->cub_file[i])
-	{
-		if (data->cub_file[i][ft_strlen(data->cub_file[i]) - 1] == '\n')
-			data->cub_file[i][ft_strlen(data->cub_file[i]) - 1] = '\0';
-		i++;
-		data->cub_file[i] = get_next_line(fd);
-	}
-	data->cub_file[i] = NULL;
-	close(fd);
-}
