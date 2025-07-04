72ed38fcc05fd560bd4aebb529a443aa1c8a446c
lowatell <lowatell@student.s19.be>
2025-07-04 13:15:53 +0200

    norm

--- srcs/engine/gun_animation.c
+++ srcs/engine/gun_animation.c
@@ -6,7 +6,7 @@
 /*   By: lowatell <lowatell@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 09:28:08 by lowatell          #+#    #+#             */
-/*   Updated: 2025/07/04 13:10:36 by lowatell         ###   ########.fr       */
+/*   Updated: 2025/07/04 13:15:15 by lowatell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,24 @@ void	swap_gun_stance(t_data *data)
 {
 	if (data->keys.mouse.fire_frames > 0 && data->player.gun.stance == 0)
 	{
-			swap_imgs(&data->player.gun.idle, &data->player.gun.firing);
-			data->keys.mouse.fire_frames = 20;
-			data->player.gun.stance = 2;
-			printf("swap to stance 2\n");
+		swap_imgs(&data->player.gun.idle, &data->player.gun.firing);
+		data->keys.mouse.fire_frames = 20;
+		data->player.gun.stance = 2;
+		printf("swap to stance 2\n");
 	}
 	if (data->keys.mouse.fire_frames <= 0 && data->player.gun.stance == 2)
 	{
-			swap_imgs(&data->player.gun.idle, &data->player.gun.firing);
-			swap_imgs(&data->player.gun.idle, &data->player.gun.moving);
-			data->keys.mouse.fire_frames = 20;
-			data->player.gun.stance = 1;
-			printf("swap to stance 1\n");
+		swap_imgs(&data->player.gun.idle, &data->player.gun.firing);
+		swap_imgs(&data->player.gun.idle, &data->player.gun.moving);
+		data->keys.mouse.fire_frames = 20;
+		data->player.gun.stance = 1;
+		printf("swap to stance 1\n");
 	}
 	if (data->keys.mouse.fire_frames <= 0 && data->player.gun.stance == 1)
 	{
-			swap_imgs(&data->player.gun.idle, &data->player.gun.moving);
-			printf("swap to stance 0\n");
-			data->player.gun.stance = 0;
+		swap_imgs(&data->player.gun.idle, &data->player.gun.moving);
+		printf("swap to stance 0\n");
+		data->player.gun.stance = 0;
 	}
 }
 
@@ -54,4 +54,3 @@ void	swap_gun(t_data *data)
 	data->player.gun = data->player.laser;
 	data->player.laser = tmp;
 }
-
